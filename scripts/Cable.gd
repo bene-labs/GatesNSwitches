416e91b1a2dc147719a38690accd7eb104b0cf6f
bene-labs <62158116+bene-labs@users.noreply.github.com>
2023-09-28 14:57:49 +0200

    add CableNode WIP

--- scripts/Cable.gd
+++ scripts/Cable.gd
@@ -14,6 +14,7 @@ export var side_offset = 26
 var connected_output = null
 var connected_input = null
 var is_hovered = false
+var node_count = 0
 
 func _ready():
 	CursorCollision.register(self)
@@ -44,26 +45,25 @@ func get_start_point():
 	return line.points[0]
 
 func set_end_point(point : Vector2):
-	line.points[1] = point
-	outline.points[1] = point
-	
-	calc_collision(outline.points[0], outline.points[-1])
+	line.points[-1] = point
+	outline.points[-1] = point
+	calc_collision(outline.points[-2], outline.points[-1], node_count)
 
-func calc_collision(start, end):
-	collision_shape.polygon[0] = start + start.direction_to(end) * side_offset + \
-		start.direction_to(end).rotated(deg2rad(-90)).normalized() * outline.width / 2
-	collision_shape.polygon[1] = start + start.direction_to(end) * side_offset + \
-		start.direction_to(end).rotated(deg2rad(90)).normalized() * outline.width / 2
-	collision_shape.polygon[2] = end + end.direction_to(start) * side_offset + \
-		start.direction_to(end).rotated(deg2rad(90)).normalized() * outline.width / 2
-	collision_shape.polygon[3] = end + end.direction_to(start) * side_offset + \
-		start.direction_to(end).rotated(deg2rad(-90)).normalized() * outline.width / 2
-	
+func calc_collision(start, end, offset = 0):
+	var idx = offset * 4
+	collision_shape.polygon.insert(idx, start + start.direction_to(end) * side_offset + \
+		start.direction_to(end).rotated(deg2rad(-90)).normalized() * outline.width / 2)
+	collision_shape.polygon.insert(idx + 1, start + start.direction_to(end) * side_offset + \
+		start.direction_to(end).rotated(deg2rad(90)).normalized() * outline.width / 2)
+	collision_shape.polygon.insert(idx + 2, end + end.direction_to(start) * side_offset + \
+		start.direction_to(end).rotated(deg2rad(90)).normalized() * outline.width / 2)
+	collision_shape.polygon.insert(idx + 3, end + end.direction_to(start) * side_offset + \
+		start.direction_to(end).rotated(deg2rad(-90)).normalized() * outline.width / 2)
 
 func is_point_inside(point) -> bool:
 	return Geometry.is_point_in_polygon(point, collision_shape.polygon)
 
-func connect_to(connection : Connection):
+func connect_to(connection):
 	if connection is Output:
 		connect_output(connection)
 	else:
@@ -79,6 +79,16 @@ func connect_output(output):
 	set_start_point(output.global_position)
 	output.connect("position_changed", self, "_on_output_position_changed")
 
+func add_cable_node(cable_node):
+	node_count += 1
+	cable_node.idx = node_count
+	cable_node.connection_type = "Input" if connected_input == null else "Output"
+	line.points.insert(node_count, cable_node.global_position)
+	outline.points.insert(node_count, cable_node.global_position)
+	calc_collision(outline.points[node_count - 1], outline.points[node_count], node_count - 1)
+	if line.points.size() > node_count + 1:
+		calc_collision(outline.points[node_count], outline.points[node_count + 1], node_count)
+
 func _on_input_position_changed(new_pos):
 	set_end_point(new_pos)
 
@@ -86,16 +96,7 @@ func _on_output_position_changed(new_pos):
 	set_start_point(new_pos)
 
 func get_end_point():
-	return line.points[1]
-
-func _exit_tree():
-	CursorCollision.unregister(self)
-	if connected_output != null:
-		connected_output.remove_cable(self)
-	if connected_input == null:
-		return
-	connected_input.connected_cable = null
-	connected_input.set_state(TriState.State.UNDEFINED)
+	return line.points[-1]
 
 func set_z_index(new_index):
 	outline.z_index = new_index
@@ -120,3 +121,12 @@ func _input(event):
 		return
 	if Input.is_action_just_pressed("destroy"):
 		queue_free()
+
+func _exit_tree():
+	CursorCollision.unregister(self)
+	if connected_output != null:
+		connected_output.remove_cable(self)
+	if connected_input == null:
+		return
+	connected_input.connected_cable = null
+	connected_input.set_state(TriState.State.UNDEFINED)
